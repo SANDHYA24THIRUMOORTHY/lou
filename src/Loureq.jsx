1b8949a480f1ebffe619b0c8d7f6e90f01afaf6b
SASHMITHA RAMESH <123223032+RSASHMITHA@users.noreply.github.com>
2023-12-18 21:35:03 +0530

    jhg

---
+++ src/Loureq.jsx
@@ -0,0 +1,112 @@
+import React, { useState } from 'react';
+import {Button,Grid} from '@mui/material';
+const Loureq = () => {
+  const [formData, setFormData] = useState({
+    applicantName: '',
+    applicantAddress: '',
+    purpose: '',
+    transactionDetails: '',
+    termsConditions: '',
+    securityRequirements: '',
+    authorizedSignatories: '',
+    legalReview: '',
+    contactInformation: '',
+    applicableLaw: '',
+    notarization: '',
+  });
+
+  const [louRequests, setLouRequests] = useState([]);
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Add logic here to handle the submission of the LOU request
+    // For example, you can update the state with the new request
+    setLouRequests([...louRequests, formData]);
+    // Clear form data after submission
+    setFormData({
+      applicantName: '',
+      applicantAddress: '',
+      purpose: '',
+      transactionDetails: '',
+      termsConditions: '',
+      securityRequirements: '',
+      authorizedSignatories: '',
+      legalReview: '',
+      contactInformation: '',
+      applicableLaw: '',
+      notarization: '',
+    });
+  };
+
+  return (
+    <div>
+      <h2>Letter of Undertaking Request</h2>
+      <form onSubmit={handleSubmit}>
+        {/* Form fields */}
+        {/* ... (same as the previous example) */}
+      </form>
+
+      {/* Display LOU requests in a table */}
+      <table>
+        <thead>
+          <tr>
+            <td>APPLICANT NAME :</td>
+            <td>
+                <label>
+                <input type="text" name="applicantName" value={formData.applicantName} onChange={handleInputChange} required />
+                </label>
+            </td>
+          </tr>
+          <br></br>
+          <tr>
+            <td>APPLICANT ADDRESS :</td>
+            <td>
+                <label>
+                <input type="text" name="applicantAddress" value={formData.applicantAddress} onChange={handleInputChange} required />
+                </label>
+            </td>
+          </tr>
+          <br></br>
+          <tr>
+            <td>PURPOSE :</td>
+            <td>
+                <label>
+                <input type="text" name="Purpose" value={formData.Purpose} onChange={handleInputChange} required />
+                </label>
+            </td>
+          </tr>
+          <br></br>
+          <tr>
+            <td>TRANSACTION DETAILS :</td>
+          </tr>
+          <br></br>
+          <tr>
+          <Grid item xs={12}>
+          <Button variant="contained" color="primary" type="submit">
+            Submit Request
+          </Button>
+        </Grid>
+          </tr>
+        </thead>
+        <tbody>
+          {louRequests.map((request, index) => (
+            <tr key={index}>
+              <td>{request.applicantName}</td>
+              <td>{request.applicantAddress}</td>
+              <td>{request.purpose}</td>
+              <td>{request.transactionDetails}</td>
+              {/* Add more cells based on your requirements */}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Loureq;
