62babee2353178b402ce2ee4dbdc50289020d1ad
HARSHAVARTHINIM <727722eucd018@skcet.ac.in>
2023-12-19 15:47:10 +0530

    hbsj

--- src/FirstPage.jsx
+++ src/FirstPage.jsx
@@ -1,15 +1,36 @@
-import React from 'react'
-import { Link} from 'react-router-dom';
+// FirstPage.js
+
+import React from 'react';
+import { Link } from 'react-router-dom';
 import './FirstPage.css';
+
 export default function FirstPage() {
+  const userLoggedIn = false; // Replace with your actual authentication logic
+
+  const handleAdminClick = () => {
+    // Implement logic for handling admin button click
+    console.log('Admin button clicked');
+  };
+
   return (
     <div className='out'>
-    <button className='sb'>ADMIN</button>
-    <br></br>
-    <br></br>
-    <Link to="/Login" ><button className='sb'>LOGIN</button></Link>
-    
-    
+      <button className='sb' onClick={handleAdminClick} aria-label="Admin">
+        ADMIN
+      </button>
+      <br></br>
+      <br></br>
+      {userLoggedIn ? (
+        <Link to="/Logout">
+          <button className='sb'>LOGOUT</button>
+        </Link>
+      ) : (
+        <>
+          <Link to="/Login">
+            <button className='sb'>LOGIN</button>
+          </Link>
+          
+        </>
+      )}
     </div>
-  )
+  );
 }
